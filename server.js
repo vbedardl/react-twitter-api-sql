1aa1a97ff6055635729f331c2404e49c6a3cd688
vbedardl <bedard_v@hotmail.fr>
2020-09-14 10:15:35 -0400

    readme file & screenshot

--- server.js
+++ server.js
@@ -146,13 +146,37 @@ app.delete("/api/follow/:id", isLoggedIn, (req, res) => {
 //Login a user
 app.post("/api/login", (req, res) => {
   const { email, password } = req.body;
+
   login(email, password)
     .then((user) => {
       req.session.userId = user.id;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+      res.json({ accessToken });
       res.send({ user });
     })
     .catch((e) => res.send());
 });
+
+// app.post("/login", authenticate, (req, res) => {
+//   const { email, password } = req.body;
+
+//   const user = { email, password };
+
+//   const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+//   res.json({ accessToken });
+// // });
+
+// function authenticate(req, res, next) => {
+//   const authHeader = req.headers['authorization']
+//   const token = authHeader && authHeader.split(' ')[1]
+//   if(token == null) return res.sendStatus(401)
+
+//   jwt.verify(token.process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+//     if(err) return res.sendStatus(403)
+//     req.user = user
+//     next()
+//   })
+// }
 
 //Logout user
 app.post("/api/logout", (req, res) => {
